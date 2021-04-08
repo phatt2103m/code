899a95b2750b5e4a14b16f454c57d3ec70009ae8
phatt2103m <81789810+phatt2103m@users.noreply.github.com>
2021-04-08 19:36:36 +0700

    lab4

---
+++ lab4/bai1-lab4.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	char kytu;
+	
+	printf("\n Nhap vao 1 ky tu : " );
+	scanf("%c", &kytu);
+	printf(" Ma ASCII cua ky tu 'A' la : %i", kytu);
+	return 0;
+}
---
+++ lab4/bai4-lap4.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+void main(int argc, char *argv[]) {
+	char kytu ;
+	printf("\nNhap mot ky tu trong bang alphabet : ");
+	
+	kytu = getchar();
+	if(kytu >= 'a' && kytu <= 'z'|| kytu >= 'A' && kytu <= 'Z'){
+	  printf(" kytu thuoc alphabet\n");
+	  
+	switch(kytu){
+	case 'a' :
+		printf(" kytu la nguyen am \n");
+	break;
+	case 'e' :
+		printf(" kytu la nguyen am \n");
+	break;
+	case 'i' :
+		printf(" kytu la nguyen am \n");
+	break;
+	case 'o' :
+		printf(" kytu la nguyen am \n");
+	break;
+    case 'u' :
+		printf(" kytu la nguyen am \n");
+	break;
+	case 'A' :
+		printf(" kytu la nguyen am \n");
+	break;
+	case 'E' :
+		printf(" kytu la nguyen am \n");
+	break;
+	case 'I' :
+		printf(" kytu la nguyen am \n");
+	break;
+	case 'O' :
+		printf(" kytu la nguyen am \n");
+	case 'U' :
+	break;
+		printf(" kytu la nguyen am \n");
+	    break;
+	
+	default:
+		printf("kytu la phu am \n");
+     	break;
+        }
+	}
+	else
+	    printf("kytu khong thuoc alphabet");
+}
