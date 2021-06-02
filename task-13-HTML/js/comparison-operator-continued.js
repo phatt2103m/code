0cebefe85c9e43429d7aae070748cd8ed0d88b4e
phatt2103m <81789810+phatt2103m@users.noreply.github.com>
2021-06-02 11:03:28 +0700

    task-13

---
+++ task-13-HTML/js/comparison-operator-continued.js
@@ -0,0 +1,7 @@
+var score1 = 90;
+var score2 = 95;
+var highScore1 = 75;
+var hightScore2 = 95;
+
+var comparison = (score1 + score2) > (highScore1 + hightScore2);
+el.textContent = 'New high score' + comparison;
